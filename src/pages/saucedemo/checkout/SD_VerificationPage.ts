992b490358b359f1566d5b790e3e410dfc55128b
Baachus <bc.robert@gmail.com>
2026-05-19 20:33:34 -0500

    added new checkout tests

--- src/pages/saucedemo/checkout/SD_VerificationPage.ts
+++ src/pages/saucedemo/checkout/SD_VerificationPage.ts
@@ -10,12 +10,36 @@ export class SD_VerificationPage extends BasePage {
   // ── Locators ────────────────────────────────────────────────────────────────
   readonly checkoutTitle: Locator;
   readonly finishBtn: Locator;
+  readonly cancelBtn: Locator;
+  readonly paymentInformationLabel: Locator;
+  readonly paymentInformation: Locator;
+  readonly shippingInformationLabel: Locator;
+  readonly shippingInformation: Locator;
+  readonly priceLabel: Locator;
+  readonly itemTotal: Locator;
+  readonly tax: Locator;
+  readonly total: Locator;
+  
+  // Items
+  readonly inventoryItem: Locator;
 
   constructor(page: Page) {
     super(page);
 
     this.checkoutTitle  = page.locator('[data-test="title"]');
     this.finishBtn = page.locator('[data-test="finish"]');
+    this.cancelBtn = page.locator('[data-test="cancel"]');
+    this.paymentInformationLabel = page.locator('[data-test="payment-info-label"]');
+    this.paymentInformation = page.locator('[data-test="payment-info-value"]');
+    this.shippingInformationLabel = page.locator('[data-test="shipping-info-label"]');
+    this.shippingInformation = page.locator('[data-test="shipping-info-value"]');
+    this.priceLabel = page.locator('[data-test="total-info-label"]')
+    this.itemTotal = page.locator('[data-test="subtotal-label"]');
+    this.tax = page.locator('[data-test="tax-label"]');
+    this.total = page.locator('[data-test="total-label"]');
+
+    // Items
+    this.inventoryItem = page.locator('[data-test="inventory-item"]');
   }
 
   // ── Navigation ──────────────────────────────────────────────────────────────
@@ -29,7 +53,9 @@ export class SD_VerificationPage extends BasePage {
   }
 
   // ── Actions ─────────────────────────────────────────────────────────────────
-
+  async getInventoryInformation(count: number): Promise<Locator> {
+    return await this.inventoryItem.nth(count);
+  }
 
   // ── Assertions ──────────────────────────────────────────────────────────────
   async assertOnLoginPage(): Promise<void> {
